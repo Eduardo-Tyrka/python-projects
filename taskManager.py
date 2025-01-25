a4a448edae8b8d0e7e53f126eaf2d55427b63164
Eduardo-Tyrka <edu.tyrka@gmail.com>
2025-01-25 11:34:23 -0300

    Update taskManager.py
    used a dictionary instead of a list to store the tasks and added a status to each task

--- taskManager.py
+++ taskManager.py
@@ -1,36 +1,38 @@
-#tasktracker
+# tasktracker 2.0
 
-tasks = []
+tasks = {}
 
 programWorking = True
 firstRun = True
 while programWorking:
-
     if firstRun:
-        print("welcome to the task manager! what do you want to do first?")
-        firstRun = False
+            print("welcome to the task manager! what do you want to do first?")
+            firstRun = False
     else:
         print("what do you want to do now?")
 
     print("add a new task (type 1)")
-    print("delete a task (select 2)")
+    print("delete a task (type 2)")
     print("see your tasks (type 3)")
-    print("close program (type 4)")
+    print("update a task status (type 4)")
+    print("close program (type 5)")
 
     userSelect = int(input())
 
     if userSelect == 1:
         print("select the name of the task")
-        tasks.append(input())
+        newTasksName = input()
+        print("select the status of the task")
+        newTasksStatus = input()
+        tasks[newTasksName] = newTasksStatus
         print("task added!")
     elif userSelect == 2:
         if len(tasks) == 0:
             print("you dont have any task")
         else:
-            print("wich task would you want ro delete? (select the number of the task)")
+            print("wich task would you want ro delete?")
             print(tasks)
-            removedTask = int(input())
-            removedTask = removedTask - 1
+            removedTask = input()
             tasks.pop(removedTask)
             print("task deleted!")
     elif userSelect == 3:
@@ -40,6 +42,17 @@ while programWorking:
             print("your tasks:")
             print(tasks)
     elif userSelect == 4:
+        if len(tasks) == 0:
+            print("you dont have any tasks")
+        else:
+            print("wich task would you want to update?")
+            print(tasks)
+            updatedTask = input()
+            print("select the new status of the task")
+            newStatus = input()
+            tasks.update({updatedTask: newStatus})
+            print("task updated")
+    elif userSelect == 5:
         print("are you sure? your tasks wont be saved, type (yes) or (no)")
         sureWantToLeave = input()
         if sureWantToLeave == "yes":
