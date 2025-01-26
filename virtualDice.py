f5ae6b6a2eed3699296cc3fc7d4ae5137dc46580
Eduardo-Tyrka <edu.tyrka@gmail.com>
2025-01-26 11:03:10 -0300

    Update virtualDice.py
    added an "answer validator" so the program wont crash if user type an invalid answer

--- virtualDice.py
+++ virtualDice.py
@@ -6,79 +6,132 @@ programWorking = True
 firstRun = True
 
 while programWorking:
+    if firstRun:
+        print("welcome to the virtual dice")
+        print("wich dice would you like to use?")
+    firstRun = False
 
-    print("welcome to the virtual dice")
-    print("wich dice would you like to use?")
     print("options: (4) (6) (8) (10) (12) (20)")
-    userSelect = int(input())
+    userSelect = input()
+
+    validAnswersStr = ["4", "6", "8", "10", "12", "20"]
+    if userSelect in validAnswersStr:
+        userSelect = int(userSelect)
+
     rollingTheDice = True
 
-    
-    if userSelect == 4:
-        while rollingTheDice:
-            print("your number is: ", random.randint(1, 4))
-            print("would you like to roll the same dice again? (s/n)")
-            sameDice = input()
-            if sameDice == "s":
-                rollingTheDice = True
-            elif sameDice == "n":
-                rollingTheDice = False
+    validAnswersInt = [4, 6, 8, 10, 12, 20]
+
+    if userSelect in validAnswersInt:
+        if userSelect == 4:
+            while rollingTheDice:
+                print("your number is: ", random.randint(1, 4))
+                print("would you like to roll the same dice again? (s/n)")
+                answeredWrong = True
+                while answeredWrong:
+                    sameDice = input()
+                    if sameDice == "s":
+                        rollingTheDice = True
+                        answeredWrong = False
+                    elif sameDice == "n":
+                        rollingTheDice = False
+                        answeredWrong = False
+                        firstRun = True
+                    else:
+                        print("select a valid answer")
+        if userSelect == 6:
+            while rollingTheDice:
+                print("your number is: ", random.randint(1, 6))
+                print("would you like to roll the same dice again? (s/n)")
+                answeredWrong = True
+                while answeredWrong:
+                    sameDice = input()
+                    if sameDice == "s":
+                        rollingTheDice = True
+                        answeredWrong = False
+                    elif sameDice == "n":
+                        rollingTheDice = False
+                        answeredWrong = False
+                        firstRun = True
+                    else:
+                        print("select a valid answer")
+        if userSelect == 8:
+            while rollingTheDice:
+                print("your number is: ", random.randint(1, 8))
+                print("would you like to roll the same dice again? (s/n)")
+                answeredWrong = True
+                while answeredWrong:
+                    sameDice = input()
+                    if sameDice == "s":
+                        rollingTheDice = True
+                        answeredWrong = False
+                    elif sameDice == "n":
+                        rollingTheDice = False
+                        answeredWrong = False
+                        firstRun = True
+                    else:
+                        print("select a valid answer")
+        if userSelect == 10:
+            while rollingTheDice:
+                print("your number is: ", random.randint(1, 10))
+                print("would you like to roll the same dice again? (s/n)")
+                answeredWrong = True
+                while answeredWrong:
+                    sameDice = input()
+                    if sameDice == "s":
+                        rollingTheDice = True
+                        answeredWrong = False
+                    elif sameDice == "n":
+                        rollingTheDice = False
+                        answeredWrong = False
+                        firstRun = True
+                    else:
+                        print("select a valid answer")
+        if userSelect == 12:
+            while rollingTheDice:
+                print("your number is: ", random.randint(1, 12))
+                print("would you like to roll the same dice again? (s/n)")
+                answeredWrong = True
+                while answeredWrong:
+                    sameDice = input()
+                    if sameDice == "s":
+                        rollingTheDice = True
+                        answeredWrong = False
+                    elif sameDice == "n":
+                        rollingTheDice = False
+                        answeredWrong = False
+                        firstRun = True
+                    else:
+                        print("select a valid answer")
+        if userSelect == 20:
+            while rollingTheDice:
+                print("your number is: ", random.randint(1, 20))
+                print("would you like to roll the same dice again? (s/n)")
+                answeredWrong = True
+                while answeredWrong:
+                    sameDice = input()
+                    if sameDice == "s":
+                        rollingTheDice = True
+                        answeredWrong = False
+                    elif sameDice == "n":
+                        rollingTheDice = False
+                        answeredWrong = False
+                        firstRun = True
+                    else:
+                        print("select a valid answer")
+
+        wantAgainAnswer = True
+        while wantAgainAnswer:
+            print("would you like to select a new dice? (s/n)")
+            tryAgain = input()
+            if tryAgain == "s":
+                programWorking = True
+                wantAgainAnswer = False
+            elif tryAgain == "n":
+                programWorking = False
+                wantAgainAnswer = False
             else:
                 print("select a valid answer")
-    elif userSelect == 6:
-        while rollingTheDice:
-            print("your number is: ", random.randint(1, 6))
-            print("would you like to roll the same dice again? (s/n)")
-            sameDice = input()
-            if sameDice == "s":
-                rollingTheDice = True
-            elif sameDice == "n":
-                rollingTheDice = False
-            else:
-                print("select a valid answer")
-    elif userSelect == 8:
-        while rollingTheDice:
-            print("your number is: ", random.randint(1, 8))
-            print("would you like to roll the same dice again? (s/n)")
-            sameDice = input()
-            if sameDice == "s":
-                rollingTheDice = True
-            elif sameDice == "n":
-                rollingTheDice = False
-            else:
-                print("select a valid answer")
-    elif userSelect == 10:
-        while rollingTheDice:
-            print("your number is: ", random.randint(1, 10))
-            print("would you like to roll the same dice again? (s/n)")
-            sameDice = input()
-            if sameDice == "s":
-                rollingTheDice = True
-            elif sameDice == "n":
-                rollingTheDice = False
-            else:
-                print("select a valid answer")
-    elif userSelect == 12:
-        while rollingTheDice:
-            print("your number is: ", random.randint(1, 12))
-            print("would you like to roll the same dice again? (s/n)")
-            sameDice = input()
-            if sameDice == "s":
-                rollingTheDice = True
-            elif sameDice == "n":
-                rollingTheDice = False
-            else:
-                print("select a valid answer")
-    elif userSelect == 20:
-        while rollingTheDice:
-            print("your number is: ", random.randint(1, 20))
-            print("would you like to roll the same dice again? (s/n)")
-            sameDice = input()
-            if sameDice == "s":
-                rollingTheDice = True
-            elif sameDice == "n":
-                rollingTheDice = False
-            else:
-                print("select a valid answer")
+
     else:
         print("select a valid answer")
